134924e193ff56801682d629f40bd87f5c6a5e50
Sergey Polischuk <spoly82@gmail.com>
2021-12-28 02:27:04 +0200

    done refresh-token task

--- src/api/auth/post.ts
+++ src/api/auth/post.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
 import { assign, pick } from 'lodash';
+import ms from 'ms';
 import { UserEntity } from '../../db/entities/user.entity';
 import JwtService from '../../services/jwt.service';
 import { wrapper } from '../../tools/wrapper.helpers';
 import { HttpError } from '../../common/errors';
+import {RefreshTokenEntity} from "../../db/entities/refresh-token.entity";
 
 export const registration = wrapper(async (req: Request, res: Response) => {
   const data = pick(req.body, 'login', 'password', 'role');
@@ -31,6 +33,40 @@ export const login = async (req: Request, res: Response) => {
   if (!user || !user.verifyPassword(password)) {
     return res.status(400).send('I dont know you bro');
   }
-  const token = JwtService.encode(user);
-  return res.send({ token });
+
+  const tokens = JwtService.makeTokens(user)
+  const refreshToken = new RefreshTokenEntity()
+  await JwtService.updateRefreshRecord(refreshToken, tokens.refreshToken)
+
+  return res.send({ tokens });
 };
+
+export const refreshToken = async (req: Request, res: Response) => {
+  const { refreshToken: tokenFromRequest } = req.body;
+
+  if (!tokenFromRequest) {
+    return res.status(400).send('Refresh token not found in request');
+  }
+
+  try {
+    const user = JwtService.decode(tokenFromRequest);
+
+    if (!user) {
+      return res.status(400).send('User not found');
+    }
+
+    const refreshToken = await RefreshTokenEntity.findOne({ where: { token: tokenFromRequest } })
+
+    if (refreshToken.expiredAt <= Date.now()) {
+      return res.status(400).send('Refresh token has been expired');
+    }
+
+    const tokens = JwtService.makeTokens(user)
+    await JwtService.updateRefreshRecord(refreshToken, tokens.refreshToken)
+    return res.send({ tokens });
+
+  } catch (err) {
+    return res.status(401).send('Invalid Token');
+  }
+
+}
